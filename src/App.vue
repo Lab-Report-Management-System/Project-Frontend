d27819d667b514fbc1e0994549fdeb06d48b82f7
Bingqi Shang <35326344+OliverShang@users.noreply.github.com>
2021-09-27 09:54:50 +0800

    Initialize deploy

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  watch: {
+    '$route.path': {
+      handler: function() {
+        if (document.getElementById('carbonads')) {
+          window._carbonads && window._carbonads.refresh()
+        }
+      }
+    }
+  }
+}
+</script>
