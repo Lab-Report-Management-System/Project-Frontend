dd75e02e545f07e3203aeb1186ca6d4d2d65b1c2
HOLLYwyh <1689394668@qq.com>
2021-10-27 10:21:34 +0800

    修改登录页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -10,7 +10,6 @@
 
         <el-form-item prop="username">
           <span class="svg-container">
-            <!--            <svg-icon icon-class="user" />-->
             <i class="el-icon-message" />
           </span>
           <el-input
@@ -48,63 +47,10 @@
 
         <div class="tips">
           <span style="margin-right:20px;">Tongji Email: 请输入同济邮箱</span>
-          <!--          <span> password: any</span>-->
         </div>
 
       </el-form>
     </el-card>
-
-
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
-
-      <div class="title-container">
-        <h3 class="title">Login Form</h3>
-      </div>
-
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="on"
-        />
-      </el-form-item>
-
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input
-          :key="passwordType"
-          ref="password"
-          v-model="loginForm.password"
-          :type="passwordType"
-          placeholder="Password"
-          name="password"
-          tabindex="2"
-          auto-complete="on"
-          @keyup.enter.native="handleLogin"
-        />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-        </span>
-      </el-form-item>
-
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
-
-      <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
-      </div>
-
-    </el-form>
-
   </div>
 </template>
 
